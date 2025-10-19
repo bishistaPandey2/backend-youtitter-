2587e5e76edb2d46315c1fd2e0c1aa67d3a71257
bishistaPandey2 <bishistapandey@gmail.com>
2025-10-20 01:05:39 +0545

    Fetch user data of subscription and subscribers

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -272,7 +272,7 @@ const updateUserAvatar = asybcHandler(async(req,res)
     const avatar = await uploadOnCloudinary(avatarLocalPath)
 
     if(!avatar.url){
-        throw new ApiError(400, 'Error while updating on avatar');        
+        throw new ApiError(400, 'Error while updating on avatar');
     }
 
 
@@ -338,6 +338,79 @@ const updateUserCover = asybcHandler(async(req,res)
         new ApiResponse(200, user, "Cover Image Update!!")
     )
 })
+
+
+const getUserChannelProfile = asyncHandler((req,res) => {
+  
+  const {username} = req.params
+
+  if (!username?.trim()){
+    throw new ApiError(400, "username is substring")
+  }
+
+  const channel = await User.aggregate([
+    {
+      $match:{
+        username: username?.toLowerCase()
+      }
+    },
+    {
+      $lookup: {
+        from: "subscriptions",   
+        localField: "_id",
+        foreignField: "channel",
+        as: "subscribers"
+      }
+    },
+    {
+      $lookup: {
+        from: "subscriptions",   
+        localField: "_id",
+        foreignField: "subscriber" ,
+        as: "subscribedTo"
+      }
+    },
+    {
+      $addFields: {
+        subscribersCount: {
+          $size: "$subscribers"
+        },
+        channelsSubscribedToCount: {
+          $size: "$subscribedTo"
+        },
+        isSubscribed:{
+          $cond: {
+            if: {$in: [req.user?._id, "$subscribers.subscriber"]},
+            then: true,
+            else: false
+          }
+        }
+      }
+    },
+    {
+      $project: {
+        fullName: 1,
+        username: 1,
+        subscribersCount: 1,
+        channelsSubscribedToCount: 1,
+        isSubscribed: 1,
+        avatar: 1,
+        coverImage: 1
+      }
+    }
+  ])
+  
+  if (!channel?.length){
+    throw new ApiError(404, "Channel doesnot exists!")
+  }
+
+  return res
+  .status(200)
+  .json(
+      new ApiResponse(200, channel[0],"User channel fetched successfully")
+    )
+})
+
 
 // Exporting
 export {
