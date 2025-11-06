d4c7eee1bdb5baa07bcf92787bccbca4ff0c2b02
bishistaPandey2 <bishistapandey@gmail.com>
2025-11-06 20:45:27 +0545

    Initial setups for like controllers.

---
+++ src/routers/likes.routes.js
@@ -0,0 +1,6 @@
+import {Router} from "express"
+import { verifyJWT } from "../middleware/auth.middleware.js";
+import {  } from "../controllers/like.controller.js";
+const router = Router()
+
+router.use(verifyJWT)
